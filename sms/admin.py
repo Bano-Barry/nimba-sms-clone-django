b212e3c1b4584e805f45b5b0d518d5c11339a57b
Bano-Barry <barrymamadoubano14@gmail.com>
2025-02-09 17:31:29 +0000

    Ajout des modèles SmsPack, Tarif, UserSmsPack et SentSms à l'interface d'administration Django

--- sms/admin.py
+++ sms/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from sms.models import SmsPack, Tarif, UserSmsPack, SentSms
+
+# Enregistrement du modèle SmsPack dans l'administration pour gérer les packs de SMS
+admin.site.register(SmsPack)
+
+# Enregistrement du modèle Tarif dans l'administration pour gérer les tarifs associés aux packs de SMS
+admin.site.register(Tarif)
+
+# Enregistrement du modèle UserSmsPack dans l'administration pour gérer les abonnements des utilisateurs aux packs de SMS
+admin.site.register(UserSmsPack)
+
+# Enregistrement du modèle SentSms dans l'administration pour gérer l'historique des SMS envoyés par les utilisateurs
+admin.site.register(SentSms)
